8a42ccf2367d092526a4009ba8457917ed775a00
EruslanovAndrei <eruslanov84as@gmail.com>
2022-10-30 15:31:23 +0300

    add methods FillArray,PrintArray,FindMax

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+int[] array = new int[6];
+void FillArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        var rnd = new Random().Next(1, 9);
+        arr[i] = rnd;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int FindMax(int[] arr)
+{
+    int max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+FillArray(array);
+PrintArray(array);
+int max = FindMax(array);
+Console.WriteLine($"Max = {max}");
